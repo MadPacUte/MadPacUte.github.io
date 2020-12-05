2a6248218188e7dd0ce107b20bdcb2e063fd26e3
Randall Madsen <randall.madsen@yahoo.com>
2020-12-05 14:16:31 -0800

    Trying to add About Us again

--- README.md
+++ README.md
@@ -1,6 +1,1 @@
-# This Repo is used for The Sauce FF league. 
-
-# https://madpacute.github.io/index.html
-
-# https://docs.google.com/document/u/0/d/1Nzg1Y0SNiEWPbjWNoX4BtxdWe6f4SQRa7V5Pdr1PpMo/mobilebasic
-
+# This is used to compile The Sauce FF site
--- utils.R
+++ utils.R
@@ -8,10 +8,34 @@ library(tidyverse)
 library(plotly)
 library(tidytext)
 library(flexdashboard)
+library(ggalt)
 
 theme_set(theme_light())
 
 options(lid = 85055)
+
+
+abbrev_to_name <-
+  tibble::tribble(
+            ~full_name, ~abbrev,
+        "Casey Walter",    "KC",
+        "Cesar Vieira",    "CV",
+       "Damien Thomas",    "D$ ",
+          "Dan Fillat",    "LM",
+       "Dominic Krohn",   "DOM",
+        "Edwin Castro",  "EDWN",
+      "Jordan Proctor",   "JJP",
+        "Jose Zavalza",   "JOE",
+          "Marc Krohn",  "MELO",
+    "Michael Gonzalez",  "MIKE",
+      "Randall Madsen",   "MAD",
+         "Tate Graham",   "MIP"
+    )
+
+
+
+
+
 
 rosters <- 
   team_roster(week = ffl_week(-1)) %>%
@@ -50,7 +74,11 @@ scores <-
          max_score = max(score),
          min_score = min(score),
          range_score = max_score - min_score) %>% 
-  ungroup()
+  ungroup() %>% 
+  mutate(abbrev = toupper(abbrev)) %>% 
+  left_join(abbrev_to_name) %>% 
+  select(-abbrev) %>%
+  rename(abbrev = full_name)
 
 data <- scores
 
@@ -125,7 +153,11 @@ power_rankings <-
   mutate(avg = round(mean(power), 1),
          abbrev = fct_reorder(abbrev, avg)) %>% 
   ungroup() %>% 
-  arrange(desc(avg))
+  arrange(desc(avg)) %>% 
+  # mutate(abbrev = toupper(abbrev)) %>%
+  left_join(abbrev_to_name, by = c("abbrev" = "full_name")) %>% 
+  select(-abbrev.y) 
+  # rename(abbrev = full_name) 
 
 power_rankings_chart <- 
     power_rankings %>% 
@@ -172,3 +204,116 @@ tie_fighter_chart <-
        x = "Average Score")
 
 
+
+weekly_historical <- 
+  fflr::week_scores(old = T) %>% 
+  bind_rows(week_scores(old = F)) %>%
+  select(-home) %>%
+  rename(name = abbrev) %>%
+  relocate(name) %>%
+  janitor::clean_names(., "upper_camel") %>%
+  arrange(Year, Week)
+
+
+yearly_historical <- 
+  fflr::score_summary(old = T) %>% 
+  bind_rows(score_summary(old = F)) %>% 
+  # filter(year == 2019) %>% 
+  select(name = abbrev, year, final, wins, losses) %>% 
+  filter(!year == 2020) %>%
+  mutate(name = toupper(name)) %>% 
+  group_by(name) %>% 
+  mutate(win_total = sum(wins),
+         loss_total = sum(losses),
+         win_percentage = round(win_total / (win_total + loss_total), 2)) %>% 
+  ungroup() %>% 
+  arrange(final) %>%
+  janitor::clean_names(., "upper_camel") %>%
+  arrange(Year, Final)
+
+
+
+
+
+yearly_historical %>% 
+  select(Name, WinTotal:WinPercentage) %>% 
+  distinct(Name, .keep_all = T) %>% 
+  left_join(power_rankings %>% 
+               select(Name = abbrev) %>% 
+               distinct())
+
+power_rankings %>% 
+  select(Name = abbrev) %>% 
+  distinct() %>% 
+  left_join(yearly_historical %>% 
+              select(Name, WinTotal:WinPercentage) %>% 
+              distinct(Name, .keep_all = T))
+
+
+
+
+
+
+
+
+standings <-
+fflr::score_summary() %>% 
+  arrange(current) %>% 
+  select(name = abbrev, draft:seed, back:streak) %>% 
+  mutate(name = toupper(as.character(name))) %>% 
+  left_join(abbrev_to_name, by = c("name" = "abbrev")) %>% 
+  select(-name) %>%
+  rename(name = full_name) %>% 
+  mutate(name = toupper(as.factor(name)),
+         name = fct_reorder(name, -draft),
+         color = case_when(current > draft ~ "43b049",
+                           current < draft ~ "d02682",
+                           TRUE ~ "80a2ad")) %>% 
+  mutate(pos_o_neg = as.character(if_else(draft - current >= 0, 1, 0)),
+         draft = draft * -1,
+         current = current * -1)
+
+
+dumbbell_chart <-
+  standings %>% 
+  arrange(desc(draft)) %>% 
+    # mutate(draft = draft * -1,
+    #        current = current * -1)  %>% 
+  ggplot(aes(x = current, xend = draft, y = name, group = name)) +
+    ggalt::geom_dumbbell(color="#a3c4dc", 
+                  colour_xend = "#d02682",
+                 size=4) +
+    # scale_color_manual(values = c("grey" = "grey", "red" = "red", "green" = "green")) +
+    labs(x = "",
+         y = "")
+
+  
+slope_chart <-
+standings %>% 
+  ggplot() + 
+    geom_segment(aes(x=1, xend=2, y=draft, yend=current, col=pos_o_neg), size=2, show.legend=F) +
+    geom_vline(xintercept=1, linetype="dashed", size=.1) + 
+    geom_vline(xintercept=2, linetype="dashed", size=.1) +
+    scale_color_manual(values = c("1"="#00ba38", "0"="#f8766d")) + # color of lines
+    labs(x = "", 
+         y = "",
+         title = "Draft Day vs. Current Standings") + # Axis labels
+    ylim(-12, 0) +
+    xlim(0.7, 2.3) +
+    # geom_text(label = paste(standings$name, abs(standings$draft), sep = ", "), y = standings$draft, x=rep(1, NROW(standings)), hjust=-0.1, size=3.5) + #left
+    geom_text(label = paste(standings$name, abs(standings$draft), sep = ", "), y = standings$draft, x=rep(1, NROW(standings)), hjust=1.1, size=1.9) + #left
+    # geom_text(label = paste(standings$name, abs(standings$current), sep = ", "), y = standings$current, x=rep(2, NROW(standings)), hjust=1.1, size=3.5) + #right
+    geom_text(label = paste(standings$name, abs(standings$current), sep = ", "), y = standings$current, x=rep(2, NROW(standings)), hjust=-0.1, size=1.9) + #right
+    geom_text(label="Draft", x=1, y=0.3*(max(standings$draft, standings$current)), hjust=1.1, size=4) + #draft label
+    geom_text(label="Current", x=2, y=0.3*(max(standings$draft, standings$current)), hjust=-0.1, size=4) + #current label 
+    theme(panel.background = element_blank(), 
+          panel.grid = element_blank(),
+          axis.ticks = element_blank(),
+          axis.text.x = element_blank(),
+          axis.text.y = element_blank(),
+          panel.border = element_blank(),
+          plot.margin = unit(c(1,2,1,2), "cm"))
+
+  
+    
+  
